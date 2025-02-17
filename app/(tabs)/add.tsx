866ab07b15fd43b58e67c9ea6b8e134409e34a25
Ravinder Singh <ravinderz@outlook.com>
2025-02-17 23:15:55 +0530

    initial project setup

---
+++ app/(tabs)/add.tsx
@@ -0,0 +1,17 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+const Add = () => {
+    // Should be able to do this by scanning QR code as well.
+    return (
+        <View>
+            <Text>Add Visitor Form</Text>
+            <Text>Name</Text>
+            <Text>Apartment No</Text>
+            <Text>Purpose</Text>
+            <Text>Date</Text>
+            <Text>Add</Text>
+        </View>
+    )
+}
+export default Add
---
+++ app/(tabs)/history.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+const History = () => {
+    return (
+        <View>
+            <Text>History</Text>
+            <Text>List of Current week visits</Text>
+        </View>
+    )
+}
+export default History
