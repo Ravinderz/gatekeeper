746d5891b9fcdee6715b2762b3c93a41fc4744f2
Ravinder Singh <ravinderz@outlook.com>
2025-02-17 23:17:41 +0530

    initial project setup

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,19 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+const Home = () => {
+    return (
+        <View>
+            <View>
+                <Text>Image</Text>
+                <Text>Apartment Name</Text>
+            </View>
+            <View>
+                <Text>Date</Text>
+                <Text>Add Pass</Text>
+                <Text>Todays Approvals/Rejections</Text>
+            </View>
+        </View>
+    )
+}
+export default Home
